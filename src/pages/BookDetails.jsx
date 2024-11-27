0b495a8b982944f158e45c20b50152eff9fd0afa
robii08 <robinvm1410@gmail.com>
2024-11-27 15:42:35 +0530

    neww

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -44,49 +44,51 @@ function BookDetails() {
 </section> */}
 
 <section className="py-md-5 py-0 pb-md-0 pb-5">
-    <div className="container">
+    <div className="container details">
         <div className="row">
             {/* Left Column - Image */}
-            <div className="col-md-6 col-12 px-md-5 d-flex align-items-md-start align-items-center px-0 order-md-1 order-1">
+            <div className="col-md-6 col-12 px-md-5 d-flex align-items-center px-0 order-md-1 order-1" style={{height:"80vh"}}>
                 <img 
                     src={`${serverurl}/uploads/${book.bookImg}`} 
                     alt={book.title || "Book cover"} 
                     className="image" 
                     width={"100%"}
-                    height={"50%"}
+                    height={"80%"}
 
                 />
             </div>
 
             {/* Right Column - Book Details */}
-            <div className="col-md-6 col-12 px-md-5 px-3 py-2 px-0 order-md-2 order-2">
-                <div className="fw-bold">
-                    <span className="fw-medium">Product Id: </span>{book._id}
-                </div>
-                <div className="fs-5 py-4">
-                    <span className="fs-1 fw-bold text-danger">₹{book.price}</span>
-                </div>
-                <h5 className="text-dark fw-bold pb-2">
-                    <span className="fw-medium">Title: </span>{book.title}
-                </h5>
-                <h5 className="text-dark fw-bold pb-2">
-                    <span className="fw-medium">Authors: </span>{book.authors}
-                </h5>
-                <h5 className="text-dark fw-bold pb-2">
-                    <span className="fw-medium">Genre: </span>{book.genre}
-                </h5>
-                <h5 className="text-dark fw-bold pb-2">
-                    <span className="fw-medium">Description: </span>{book.description}
-                </h5>
-
-                {/* Buttons */}
-                <div className="d-flex flex-wrap justify-content-between mt-5">
-                    <a 
-                        className="btn btn-outline-danger px-md-4 w-100 w-md-auto mb-2 mb-md-0 d-flex justify-content-center align-items-center"
-                    >
-                        <i className="fa-solid fa-heart me-2"></i>Buy
-                    </a>
-                    
+            <div className="col-md-6 col-12 px-md-5 d-flex align-items-center px-3 py-2 px-0 order-md-2 order-2">
+                <div>
+                    <div className="fw-bold">
+                        <span className="fw-medium">Product Id: </span>{book._id}
+                    </div>
+                    <div className="fs-5 py-4">
+                        <span className="fs-1 fw-bold text-danger">₹{book.price}</span>
+                    </div>
+                    <h5 className="text-dark fw-bold pb-2">
+                        <span className="fw-medium">Title: </span>{book.title}
+                    </h5>
+                    <h5 className="text-dark fw-bold pb-2">
+                        <span className="fw-medium">Authors: </span>{book.authors}
+                    </h5>
+                    <h5 className="text-dark fw-bold pb-2">
+                        <span className="fw-medium">Genre: </span>{book.genre}
+                    </h5>
+                    <h5 className="text-dark fw-bold pb-2">
+                        <span className="fw-medium">Description: </span>{book.description}
+                    </h5>
+    
+                    {/* Buttons */}
+                    <div className="d-flex flex-wrap justify-content-between mt-5">
+                        <a 
+                            className="btn btn-outline-danger px-md-4 w-100 w-md-auto mb-2 mb-md-0 d-flex justify-content-center align-items-center"
+                        >
+                            <i className="fa-solid fa-heart me-2"></i>Buy
+                        </a>
+                        
+                    </div>
                 </div>
             </div>
         </div>
